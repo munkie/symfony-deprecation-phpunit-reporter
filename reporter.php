575ad8998a0815ba482d059be1e9fcc9941efc9a
Mikhail Shamin <munk13@gmail.com>
2015-12-29 18:12:23 +0700

    Rename to DeprecationListener

--- reporter.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__.'/src/DeprecationReporter.php';
-
-class deprecation extends \Deprecation\DeprecationReporter {}
--- src/DeprecationListener.php
+++ src/DeprecationListener.php
@@ -9,77 +9,62 @@ use PHPUnit_Framework_AssertionFailedError;
 use PHPUnit_Framework_Test;
 use PHPUnit_Framework_TestSuite;
 
-class DeprecationReporter extends \PHPUnit_Util_Printer implements \PHPUnit_Framework_TestListener
+class DeprecationListener extends \PHPUnit_Util_Printer implements \PHPUnit_Framework_TestListener
 {
     /**
      * @var DeprecationErrorHandler
      */
     private $handler;
 
-    /**
-     * @var \PHPUnit_Util_Printer
-     */
-    private $printer;
-
-    public function __construct($out)
+    public function __construct()
     {
-        parent::__construct($out);
+        parent::__construct(null);
 
         $this->handler = DeprecationErrorHandler::register();
-        $this->printer = new \PHPUnit_TextUI_ResultPrinter($out);
     }
 
     public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
-        $this->printer->addError($test, $e, $time);
     }
 
     public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
     {
-        $this->printer->addFailure($test, $e, $time);
     }
 
     public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
-        $this->printer->addIncompleteTest($test, $e, $time);
     }
 
     public function addRiskyTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
-        $this->printer->addRiskyTest($test, $e, $time);
     }
 
     public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
-        $this->printer->addSkippedTest($test, $e, $time);
     }
 
     public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
     {
-        $this->printer->startTestSuite($suite);
     }
 
     public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
     {
-        $this->printer->endTestSuite($suite);
     }
 
     public function startTest(PHPUnit_Framework_Test $test)
     {
-        $this->printer->startTest($test);
     }
 
     public function endTest(PHPUnit_Framework_Test $test, $time)
     {
-        $this->printer->endTest($test, $time);
     }
 
     public function flush()
     {
+        parent::flush();
+
         $this->saveCheckstyleReport();
         $this->printOutputReport();
-
-        parent::flush();
     }
 
     private function saveCheckstyleReport()
@@ -97,4 +82,5 @@ class DeprecationReporter extends \PHPUnit_Util_Printer implements \PHPUnit_Fram
         $report = new Output();
         $report->report($this->handler->getDeprecations());
     }
+
 }
